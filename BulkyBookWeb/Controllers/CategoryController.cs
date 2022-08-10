04b942a59a84ff931d9782cade1b748d7d8606e1
Johnny Hildebrand <JHVJD1@gmail.com>
2022-08-09 20:14:58 -0500

    Add Notification partial view to create, edit and delete add notification view

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -40,6 +40,7 @@ public class CategoryController : Controller
 						{
 						_db.Categories.Add(obj);
 						_db.SaveChanges();
+						TempData["success"] = "Category created successfully";
 						return RedirectToAction("Index");
 						}
 				return View(obj);
@@ -76,9 +77,47 @@ public class CategoryController : Controller
 						{
 						_db.Categories.Update(obj);
 						_db.SaveChanges();
+						TempData["success"] = "Category edited successfully";
 						return RedirectToAction("Index");
 						}
 				return View(obj);
+				}
+
+		//GET
+		public IActionResult Delete(int? id)
+				{
+				if (id == null || id == 0)
+						{
+						return NotFound();
+						}
+				var categoryFromDb = _db.Categories.Find(id);
+				/*	var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+					var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);*/
+				if (categoryFromDb == null)
+						{
+						return NotFound();
+						}
+
+				return View(categoryFromDb);
+				}
+
+		//POST
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePOST(int? id)
+				{
+				var obj = _db.Categories.Find(id);
+				if (obj == null)
+						{
+						return NotFound();
+						}
+
+				_db.Categories.Remove(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category deleted successfully";
+				return RedirectToAction("Index");
+
+
 				}
 
 		}
